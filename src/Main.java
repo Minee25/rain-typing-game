0c11740025b98ad1bc2d675f6913d00ef3d553af
Minee <oakmuansuk123@gmail.com>
2025-10-03 04:01:14 +0700

    Change file structure

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,9 @@
 import javax.swing.*;
-import java.awt.*;
-import java.io.File;
 
-public class Main extends JFrame {
-  public Main() {
-    setTitle("Rain Typeing Game");
-    setSize(1280, 729);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLocationRelativeTo(null);
-
-    Image logo = new ImageIcon(System.getProperty("user.dir") + File.separator + "../assets/img/logo64.png").getImage();
-    setIconImage(logo);
-
-    add(new MainMenu());
-  }
-
+public class Main {
   public static void main(String[] args) {
     SwingUtilities.invokeLater(() -> {
-      new Main().setVisible(true);
+      new RainTypingWindows().setVisible(true);
     });
   }
 }
---
+++ src/RainTypingWindows.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import javax.swing.*;
+import java.awt.*;
+
+public class RainTypingWindows extends JFrame {
+  public RainTypingWindows() {
+    setTitle("Rain Typeing Game");
+    setSize(1280, 729);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLocationRelativeTo(null);
+
+    Image logo = new ImageIcon(System.getProperty("user.dir") + File.separator + "../assets/img/logo64.png").getImage();
+    setIconImage(logo);
+
+    add(new MainMenu());
+  }
+}
